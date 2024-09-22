cda729214018e3e7b0be6d99c12b258c5d6d3bfa
Gordey Dovydenko <inostaziss@gmail.com>
2024-09-22 18:02:02 +0300

    fix

---
+++ src/main/java/org/example/scheduleevent/rest/controller/util/CustomSwaggerConfigController.java
@@ -0,0 +1,23 @@
+package org.example.scheduleevent.rest.controller.util;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class CustomSwaggerConfigController {
+
+    @GetMapping("/v3/api-docs/swagger-config")
+    public Map<String, String> customSwaggerConfig() {
+
+        Map<String, String> defaultConfig = new HashMap<>();
+        defaultConfig.put("oauth2RedirectUrl", "http://api.quqee.tech/swagger-ui/oauth2-redirect.html");
+        defaultConfig.put("configUrl", "/v3/api-docs/swagger-config");
+        defaultConfig.put("url", "/v3/api-docs");
+        defaultConfig.put("validatorUrl", "");
+
+        return defaultConfig;
+    }
+}
