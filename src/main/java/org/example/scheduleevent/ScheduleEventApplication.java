d99fc10e95c792a75f4102da2bc2b860cfa625a4
Gordey Dovydenko <inostaziss@gmail.com>
2024-09-22 13:19:24 +0300

    fix

--- src/main/java/org/example/scheduleevent/ScheduleEventApplication.java
+++ src/main/java/org/example/scheduleevent/ScheduleEventApplication.java
@@ -15,3 +15,11 @@ public class ScheduleEventApplication {
     }
 
 }
+
+/*
+docker exec -it 9556682afb1a bash
+cd /opt/keycloak/bin/
+./kcadm.sh config credentials --server http://localhost:8080 --realm master --user admin
+./kcadm.sh update realms/master -s sslRequired=NONE
+./kcadm.sh update realms/hits-project -s sslRequired=NONE
+*/
